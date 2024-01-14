9e70556312d46b14024596bf2158dc1abdf7b058
Kimicu <kitgunplay@gmail.com>
2024-01-14 18:45:58 +0300

    feat: utils [ static coroutines ]

---
+++ Runtime/Utils/Coroutine/Coroutine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace KimicuYandexGames.Utils
+{
+    public class Coroutine
+    {
+        private UnityEngine.Coroutine _routine;
+
+        /// <summary> Start new coroutine. </summary>
+        public void StartRoutine(IEnumerator enumerator)
+        {
+            StopRoutine();
+            _routine = Coroutines.StartRoutine(enumerator);
+        }
+
+        /// <summary> Try start new coroutine. </summary>
+        public bool TryStartRoutine(IEnumerator enumerator)
+        {
+            if (_routine != null) return false;
+            _routine = Coroutines.StartRoutine(enumerator);
+            return true;
+        }
+
+        /// <summary> Stop current coroutine. </summary>
+        public void StopRoutine()
+        {
+            if (_routine == null) return;
+            Coroutines.StopRoutine(_routine);
+            _routine = null;
+        }
+    }
+}
---
+++ Runtime/Utils/Coroutine/Coroutines.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace KimicuYandexGames.Utils
+{
+    public class Coroutines : MonoBehaviour
+    {
+        private static Coroutines _instance;
+
+        private static Coroutines Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    GameObject go = new("[COROUTINES]");
+                    _instance = go.AddComponent<Coroutines>();
+                    DontDestroyOnLoad(go);
+                }
+
+                return _instance;
+            }
+        }
+
+        public static UnityEngine.Coroutine StartRoutine(IEnumerator enumerator) => Instance.StartCoroutine(enumerator);
+        public static void StopRoutine([NotNull] UnityEngine.Coroutine routine) => Instance.StopCoroutine(routine);
+    }
+}
