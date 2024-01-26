52cbf4f10ae2a962cbe192bd728482bce7c28e56
Kimicu <kitgunplay@gmail.com>
2024-01-26 17:43:41 +0300

    feat: import WebApplication in Advertisement.cs

--- Runtime/Advertisement.cs
+++ Runtime/Advertisement.cs
@@ -11,7 +11,7 @@ namespace Kimicu.YandexGames
     {
         public static bool AdvertisementIsAvailable { get; private set; } = true;
 
-        private static bool _initialized = false;
+        public static bool Initialized { get; private set; } = false;
 
         private static readonly Coroutine ReloadCoroutine = new Coroutine();
 
@@ -20,15 +20,34 @@ namespace Kimicu.YandexGames
         public static void Initialize()
         {
             ReloadCoroutine.StartRoutine(AdvertisementReloadRoutine());
-            _initialized = true;
+            Initialized = true;
         }
 
         public static void ShowInterstitialAd(Action onOpenCallback = null, Action onCloseCallback = null, Action<string> onErrorCallback = null, Action onOfflineCallback = null)
         {
-            if (!_initialized) throw new Exception($"{nameof(Advertisement)} not initialized!");
-            
+            if (!Initialized) throw new Exception($"{nameof(Advertisement)} not initialized!");
+
             #if !UNITY_EDITOR && UNITY_WEBGL
-            Agava.YandexGames.InterstitialAd.Show(onOpenCallback, (wasShown) => onCloseCallback?.Invoke(), onErrorCallback, onOfflineCallback);
+            Agava.YandexGames.InterstitialAd.Show(() =>
+                {
+                    onOpenCallback?.Invoke();
+                    WebApplication.InAdvert = true;
+                },
+                (_) =>
+                {
+                    onCloseCallback?.Invoke();
+                    WebApplication.InAdvert = false;
+                },
+                (error) =>
+                {
+                    onErrorCallback?.Invoke(error);
+                    WebApplication.InAdvert = false;
+                },
+                () =>
+                {
+                    onOfflineCallback?.Invoke();
+                    WebApplication.InAdvert = false;
+                });
             #elif UNITY_EDITOR
             onOpenCallback?.Invoke();
             onCloseCallback?.Invoke();
@@ -37,10 +56,24 @@ namespace Kimicu.YandexGames
 
         public static void ShowVideoAd(Action onOpenCallback = null, Action onRewardedCallback = null, Action onCloseCallback = null, Action<string> onErrorCallback = null)
         {
-            if (!_initialized) throw new Exception($"{nameof(Advertisement)} not initialized!");
-            
+            if (!Initialized) throw new Exception($"{nameof(Advertisement)} not initialized!");
+
             #if !UNITY_EDITOR && UNITY_WEBGL
-            Agava.YandexGames.VideoAd.Show(onOpenCallback, onRewardedCallback, onCloseCallback, onErrorCallback);
+            Agava.YandexGames.VideoAd.Show(() =>
+                {
+                    onOpenCallback?.Invoke();
+                    WebApplication.InAdvert = true;
+                }, ()
+                    => onRewardedCallback?.Invoke(),
+                () =>
+                {
+                    onCloseCallback?.Invoke();
+                    WebApplication.InAdvert = false;
+                }, (error) =>
+                {
+                    onErrorCallback?.Invoke(error);
+                    WebApplication.InAdvert = false;
+                });
             #elif UNITY_EDITOR
             onOpenCallback?.Invoke();
             onRewardedCallback?.Invoke();
@@ -50,8 +83,8 @@ namespace Kimicu.YandexGames
 
         public static void StickySetActive(bool value)
         {
-            if (!_initialized) throw new Exception($"{nameof(Advertisement)} not initialized!");
-            
+            if (!Initialized) throw new Exception($"{nameof(Advertisement)} not initialized!");
+
             #if !UNITY_EDITOR && UNITY_WEBGL
             if (value) Agava.YandexGames.StickyAd.Show();
             else Agava.YandexGames.StickyAd.Hide();
