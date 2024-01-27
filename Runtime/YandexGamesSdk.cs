149a248048c317f8cda7527810c24f498751a6df
Kimicu <kitgunplay@gmail.com>
2024-01-27 13:40:00 +0300

    feat: Yandex Games Sdk [ Is Running Yandex, Callback Logging, Is Initialized, Environment, Game Ready(), Initialize() ]

---
+++ Runtime/YandexGamesSdk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using Agava.YandexGames;
+using UnityEngine;
+
+namespace Kimicu.YandexGames
+{
+    public static partial class YandexGamesSdk
+    {
+        public static bool IsRunningOnYandex => Agava.YandexGames.YandexGamesSdk.IsRunningOnYandex;
+        public static bool CallbackLogging => Agava.YandexGames.YandexGamesSdk.CallbackLogging;
+        public static bool IsInitialized => Agava.YandexGames.YandexGamesSdk.IsInitialized;
+        public static YandexGamesEnvironment Environment => Agava.YandexGames.YandexGamesSdk.Environment;
+
+        public static void GameReady()
+        {
+            #if !UNITY_EDITOR && UNITY_WEBGL
+            Agava.YandexGames.YandexGamesSdk.GameReady();
+            #else
+            Debug.Log($"GameReady invoke!");
+            #endif
+        }
+
+        public static IEnumerator Initialize(Action onSuccessCallback = null)
+        {
+            #if !UNITY_EDITOR && UNITY_WEBGL
+            yield return Agava.YandexGames.YandexGamesSdk.Initialize(onSuccessCallback);
+            #else
+            onSuccessCallback?.Invoke();
+            yield break;
+            #endif
+        }
+    }
+}
