77d4d84f7185f099bb30736704a4a05e90c92b90
Kimicu <kitgunplay@gmail.com>
2024-02-13 22:47:18 +0300

    feat: added function IsMobile in Device.cs

---
+++ Runtime/Device.cs
@@ -0,0 +1,15 @@
+﻿namespace Kimicu.YandexGames
+{
+    public static class Device
+    {
+        public static bool IsMobile
+        {
+            get
+#if !UNITY_EDITOR && UNITY_WEBGL
+                => Agava.WebUtility.Device.IsMobile;
+#else
+                => true;
+#endif
+        }
+    }
+}
