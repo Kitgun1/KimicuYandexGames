6cee2939ce933e7b00678bd9e18a925a74bb1889
Kimicu <kitgunplay@gmail.com>
2024-02-13 22:47:39 +0300

    feat: added ad block checker in AdBlock.cs

---
+++ Runtime/AdBlock.cs
@@ -0,0 +1,13 @@
+﻿namespace Kimicu.YandexGames
+{
+    public static class AdBlock
+    {
+        /// <returns> Status of an AdBlock addon in the browser. </returns>
+        public static bool Enabled =>
+#if !UNITY_EDITOR && UNITY_WEBGL
+            Agava.WebUtility.AdBlock.Enabled;
+#else
+            false;
+#endif
+    }
+}
