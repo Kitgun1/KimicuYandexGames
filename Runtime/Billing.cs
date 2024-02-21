46cf5862c7a0fb3dc4b609c93bb1f915cbced773
Kimicu <kitgunplay@gmail.com>
2024-02-21 15:15:02 +0300

    feat: remove field purchased products and added method get purchased products

--- Runtime/Billing.cs
+++ Runtime/Billing.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Agava.YandexGames;
+using UnityEngine;
 #if !UNITY_EDITOR && UNITY_WEBGL
 using UnityEngine;
 #endif
@@ -13,16 +15,17 @@ namespace Kimicu.YandexGames
         private static CatalogProduct[] _catalogProducts;
         private static GetPurchasedProductsResponse _getPurchasedProductsResponse;
 
-        #if !UNITY_EDITOR && UNITY_WEBGL
+#if !UNITY_EDITOR && UNITY_WEBGL
         private static bool _productCatalogSuccesses;
         private static bool _purchasedProductsSuccesses;
-        #endif
+#endif
+        private static bool _relevancePurchaseProductData = false;
+        
+        private static IEnumerable<PurchasedProduct> PurchasedProducts => _getPurchasedProductsResponse.purchasedProducts;
 
         public static bool Initialized { get; private set; } = false;
-        
-        public static IEnumerable<CatalogProduct> CatalogProducts => _catalogProducts;
-        public static IEnumerable<PurchasedProduct> PurchasedProducts => _getPurchasedProductsResponse.purchasedProducts;
 
+        public static IEnumerable<CatalogProduct> CatalogProducts => _catalogProducts;
         public static string PurchasedSignature => _getPurchasedProductsResponse.signature;
 
         public static IEnumerator Initialize(Action onSuccessCallback = null)
@@ -40,7 +43,7 @@ namespace Kimicu.YandexGames
             onSuccessCallback?.Invoke();
         }
 
-        #if !UNITY_EDITOR && UNITY_WEBGL
+#if !UNITY_EDITOR && UNITY_WEBGL
         private static void OnGetProductCatalogSuccessCallback(GetProductCatalogResponse response)
         {
             _catalogProducts = response.products;
@@ -51,34 +54,59 @@ namespace Kimicu.YandexGames
         {
             _getPurchasedProductsResponse = response;
             _purchasedProductsSuccesses = true;
+            _relevancePurchaseProductData = true;
         }
 
         private static void OnErrorCallback(string error) => Debug.LogError($"Billing error: {error}");
-        #endif
+#endif
+
+        public static void GetPurchasedProducts(Action<PurchasedProduct[]> onSuccessCallback = null, Action<string> onErrorCallback = null)
+        {
+            if (_relevancePurchaseProductData)
+            {
+                onSuccessCallback?.Invoke(PurchasedProducts.ToArray());
+                return;
+            }
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+            Agava.YandexGames.Billing.GetPurchasedProducts((response) =>
+            {
+                OnGetPurchasedProductsSuccessCallback(response);
+                onSuccessCallback?.Invoke(response.purchasedProducts.ToArray());
+            }, (error) =>
+            {
+                OnErrorCallback(error);
+                onErrorCallback?.Invoke(error);
+            });
+#else
+#endif
+        }
 
         public static void PurchaseProduct(string productId, Action<PurchaseProductResponse> onSuccessCallback = null, Action<string> onErrorCallback = null, string developerPayload = "")
         {
-            #if !UNITY_EDITOR && UNITY_WEBGL
+#if !UNITY_EDITOR && UNITY_WEBGL
+            _purchasedProductsSuccesses = false;
+            _relevancePurchaseProductData = false;
             WebApplication.InPurchaseWindow = true;
-            Agava.YandexGames.Billing.PurchaseProduct(productId,(response) =>
+            Agava.YandexGames.Billing.PurchaseProduct(productId, (response) =>
             {
-                onSuccessCallback?.Invoke(response);
                 WebApplication.InPurchaseWindow = false;
                 Agava.YandexGames.Billing.GetPurchasedProducts(OnGetPurchasedProductsSuccessCallback, OnErrorCallback);
-            }, (error)=>
+                onSuccessCallback?.Invoke(response);
+            }, (error) =>
             {
-                onErrorCallback?.Invoke(error);
                 WebApplication.InPurchaseWindow = false;
+                onErrorCallback?.Invoke(error);
             }, developerPayload);
-            #else
+#else
             // TODO: Добавить тестовые подтверждения покупок для UNITY_EDITOR
-            #endif
+#endif
         }
 
         public static void ConsumeProduct(string purchasedProductToken, Action onSuccessCallback = null, Action<string> onErrorCallback = null)
         {
-            #if !UNITY_EDITOR && UNITY_WEBGL
-            Agava.YandexGames.Billing.ConsumeProduct(purchasedProductToken, ()=>
+#if !UNITY_EDITOR && UNITY_WEBGL
+            Agava.YandexGames.Billing.ConsumeProduct(purchasedProductToken, () =>
             {
                 onSuccessCallback?.Invoke();
                 Agava.YandexGames.Billing.GetPurchasedProducts(OnGetPurchasedProductsSuccessCallback, OnErrorCallback);
@@ -87,9 +115,9 @@ namespace Kimicu.YandexGames
                 onErrorCallback?.Invoke(error);
                 Agava.YandexGames.Billing.GetPurchasedProducts(OnGetPurchasedProductsSuccessCallback, OnErrorCallback);
             });
-            #else
+#else
             // TODO: Добавить тестовые покупки для UNITY_EDITOR
-            #endif
+#endif
         }
     }
 }
