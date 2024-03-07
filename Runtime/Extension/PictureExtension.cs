f424986a925b4dffa2e3f6217078ff1be2eaf191
Kimicu <kitgunplay@gmail.com>
2024-03-07 16:19:37 +0300

    feat: picture extension

---
+++ Runtime/Extension/PictureExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Kimicu.YandexGames.Extension
+{
+    public static class PictureExtension
+    {
+        public static IEnumerator GetPicture(this string url, Action<Texture2D> onSuccessCallback,
+            Action<string> onError = null)
+        {
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+            yield return www.SendWebRequest();
+            if (www.result is UnityWebRequest.Result.ConnectionError or UnityWebRequest.Result.ProtocolError)
+            {
+                onError?.Invoke(www.error);
+            }
+            else
+            {
+                Texture2D texture2D = DownloadHandlerTexture.GetContent(www);
+                onSuccessCallback?.Invoke(texture2D);
+            }
+        }
+    }
+}
