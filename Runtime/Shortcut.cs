ade90d4ab434eab976a8856cd237c19f3c4252f1
Kimicu <kitgunplay@gmail.com>
2024-03-08 10:55:36 +0300

    feat: update

---
+++ Runtime/Shortcut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KimicuYandexGames
+{
+    public static class Shortcut
+    {
+        public static void CanSuggest(Action<bool> onResultCallback)
+        {
+            #if !UNITY_EDITOR && UNITY_WEBGL
+            Agava.YandexGames.Shortcut.CanSuggest(onResultCallback);
+            #endif
+        }
+
+        public static void Suggest(Action<bool> onResultCallback = null)
+        {
+            #if !UNITY_EDITOR && UNITY_WEBGL
+            Agava.YandexGames.Shortcut.Suggest(onResultCallback);
+            #endif
+        }
+    }
+}
