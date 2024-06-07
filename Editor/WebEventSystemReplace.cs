5c9be787877852ca21e7b9928fae92ccb27a75af
Kimicu <kitgunplay@gmail.com>
2024-06-07 07:56:09 +0300

    feat: web event system replacer

---
+++ Editor/WebEventSystemReplace.cs
@@ -0,0 +1,47 @@
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using System.Linq;
+using Agava.WebUtility;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace KimicuYandexGames.Editors
+{
+    public class WebEventSystemReplace : Editor
+    {
+        [MenuItem("Kimicu/Yandex Games/Replace All Event Systems")]
+        private static void ReplaceEventSystems()
+        {
+            var scenes = EditorBuildSettings.scenes.Select(scene => scene.path).ToList();
+
+            Debug.Log($"scenes: {scenes.Count}");
+            foreach (var scene in scenes)
+            {
+                Debug.Log($"scene: {scene}");
+                EditorSceneManager.OpenScene(scene);
+
+                var allGameObjects = Resources.FindObjectsOfTypeAll<EventSystem>();
+                foreach (EventSystem eventSystem in allGameObjects)
+                {
+                    var obj = eventSystem.gameObject;
+                    if (eventSystem != null)
+                    {
+                        DestroyImmediate(obj.GetComponent<StandaloneInputModule>());
+                        DestroyImmediate(eventSystem);
+                        
+                        obj.AddComponent<WebEventSystem>();
+                        obj.AddComponent<StandaloneInputModule>();
+                        EditorUtility.SetDirty(obj);
+                    }
+                }
+
+                EditorSceneManager.SaveScene(SceneManager.GetSceneByPath(scene));
+                EditorSceneManager.CloseScene(SceneManager.GetSceneByPath(scene), false);
+            }
+        }
+    }
+}
+#endif
