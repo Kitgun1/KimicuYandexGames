592e65d60949bc3ad2e925b33e5ee1f78950565d
A.Renkoff <lex.keivi@mail.ru>
2025-02-19 14:35:41 +0500

    Updated REVIEW POPUP

--- Agava/YandexGames/Runtime/Tools/ReviewPopup.cs
+++ Agava/YandexGames/Runtime/Tools/ReviewPopup.cs
@@ -10,23 +10,11 @@ namespace Agava.YandexGames
         private static Action<bool, string> s_onCanOpenCallback;
         private static Action<bool> s_onOpenCallback;
 
-        public static void CanOpen(Action<bool, string> onResultCallback)
+        public static void RequestCanOpen(Action<bool, string> onResultCallback)
         {
             s_onCanOpenCallback = onResultCallback;
 
             ReviewPopupCanOpen(OnCanOpenCallback);
-        }
-
-        [DllImport("__Internal")]
-        private static extern void ReviewPopupCanOpen(Action<bool, string> onResultCallback);
-
-        [MonoPInvokeCallback(typeof(Action<bool, string>))]
-        private static void OnCanOpenCallback(bool result, string reason)
-        {
-            if (YandexGamesSdk.CallbackLogging)
-                Debug.Log($"{nameof(Shortcut)}.{nameof(OnCanOpenCallback)} called. {nameof(result)}={result} {nameof(reason)}={reason}");
-
-            s_onCanOpenCallback?.Invoke(result, reason);
         }
 
         public static void Open(Action<bool> onResultCallback = null)
@@ -36,8 +24,20 @@ namespace Agava.YandexGames
             ReviewPopupOpen(OnOpenCallback);
         }
 
-        [DllImport("__Internal")]
+		[DllImport("__Internal")]
+        private static extern void ReviewPopupCanOpen(Action<bool, string> onResultCallback);
+
+		[DllImport("__Internal")]
         private static extern void ReviewPopupOpen(Action<bool> onResultCallback);
+
+		[MonoPInvokeCallback(typeof(Action<bool, string>))]
+        private static void OnCanOpenCallback(bool result, string reason)
+        {
+            if (YandexGamesSdk.CallbackLogging)
+                Debug.Log($"{nameof(Shortcut)}.{nameof(OnCanOpenCallback)} called. {nameof(result)}={result} {nameof(reason)}={reason}");
+
+            s_onCanOpenCallback?.Invoke(result, reason);
+        }
 
         [MonoPInvokeCallback(typeof(Action<bool>))]
         private static void OnOpenCallback(bool result)
